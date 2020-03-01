a4455de892e88675d7dd2f3b0f1886dd8609371e
mhagdorn <magnus.hagdorn@marsupium.org>
2020-03-01 12:37:41 +0000

    add setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name='bliptools',
+      install_requires = ['requests',
+                          'sqlalchemy',],
+      version='0.0.1',
+      description='tools for handling blipfoto journals',
+      author='Magnus Hagdorn',
+      author_email='magnus.hagdorn@marsupium.org',
+      url='https://github.com/mhagdorn/bliptools',
+      packages=['bliptools'],
+      entry_points={
+          'console_scripts': [
+              'bliptools-update = bliptools.update:main',
+              ],
+      },
+  )
