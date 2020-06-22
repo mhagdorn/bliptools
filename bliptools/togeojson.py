450c5cb3d55c274d37eeee116baf7549fd2d1e40
mhagdorn <magnus.hagdorn@marsupium.org>
2020-06-22 22:31:03 +0100

    add option to select a date range for which to extract data

--- bliptools/togeojson.py
+++ bliptools/togeojson.py
@@ -4,6 +4,7 @@ import logging
 import argparse
 import pathlib
 import geojson
+import datetime
 
 from .model import *
 from .config import *
@@ -13,6 +14,8 @@ def main():
     cfg = pathlib.Path('~/.bliptools')
     parser = argparse.ArgumentParser()
     parser.add_argument('input',metavar='NAME',help='read journal data from file NAME')
+    parser.add_argument('-s','--start-date',metavar='YYYY-MM-DD',help='the start date from which to extract data')
+    parser.add_argument('-e','--end-date',metavar='YYYY-MM-DD',help='the end date until which to extract data')
     parser.add_argument('--output','-o',metavar='NAME',help='write geojson to file NAME')
     parser.add_argument('-c','--config',metavar='CFG',type=pathlib.Path,default=cfg,help='read configuration from CFG, default {}'.format(cfg))
     parser.add_argument('--verbose',action='store_true',default=False,help='be verbose')
@@ -27,9 +30,22 @@ def main():
     logging.basicConfig(format='%(levelname)s:%(message)s', level=level)
     cfg = readConfig(args.config)
 
+    start = None
+    if args.start_date is not None:
+        try:
+            start = datetime.datetime.strptime(args.start_date,'%Y-%m-%d').date()
+        except Exception as e:
+            parser.error('Cannot parse start date: {}'.format(e))
+    end = None
+    if args.end_date is not None:
+        try:
+            end = datetime.datetime.strptime(args.end_date,'%Y-%m-%d').date()
+        except Exception as e:
+            parser.error('Cannot parse end date: {}'.format(e))
+            
     db = BlipDB('sqlite:///{}'.format(args.input))
     features=[]
-    for e in db.get_entries_with_location():
+    for e in db.get_entries_with_location(start=start,end=end):
         feature = geojson.Feature(geometry=geojson.Point((e.lon,e.lat)),
                                   properties = {'img':e.image_url,
                                                 'title':e.title,
